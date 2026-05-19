fc27fef0a833e8518fe76149267d968b5684ffde
kathirvelanenergies <kathirvelanenergies@gmail.com>
2026-05-19 12:22:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+export default function App(){
+return(
+<div style={{fontFamily:'Arial',padding:'40px'}}>
+<h1>Kathirvelan Energies</h1>
+<h3>Powering Industries with Clean Solar Energy</h3>
+<p>Supplying energy units to factories, malls and businesses.</p>
+<p>Email: kathirvelanenergies@gmail.com</p>
+<p>Phone: +91 9025467490</p>
+<p>Location: Chennai, Tamil Nadu</p>
+<img src="/ke-logo.png" width="220"/>
+</div>
+)}
